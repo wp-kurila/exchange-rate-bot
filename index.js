4697c0d2b87e2e0f7f37cd40a426bce9e11dd7c2
Курилин Никита Андреевич <kurilinna@pochtabank.ru>
2023-12-30 16:27:46 +0300

    add Rub rate

--- index.js
+++ index.js
@@ -4,21 +4,71 @@ const token = '6355764517:AAGSsWSTnqzsNY3G4UcysNCM2bd7W7DhOgg';
 
 const bot = new TelegramApi(token, {polling: true});
 
+const cryptoData = [
+	{
+		url: 'https://www.commex.com/bapi/c2c/v1/friendly/c2c/ad/search',
+		body: {
+			asset: "USDT",
+			fiat: "RUB",
+			page: 1,
+			rows: 1,
+			tradeType: "BUY"
+		}
+	},
+	{
+		url: 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
+		body: {
+			"fiat": "THB",
+			"page": 1,
+			"rows": 1,
+			"tradeType": "BUY",
+			"asset": "USDT"
+		}
+	}
+];
+
+const getData = async () => {
+	const requests = cryptoData.map(item => fetch(item.url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8'
+		},
+		body: JSON.stringify(item.body)
+	}));
+
+	const [r1, r2] = await Promise.all(requests);
+
+	return [await r1.json(), await r2.json()];
+}
+
 const start = () => {
 	bot.setMyCommands([
 		{command: '/info', description: 'Получить курс обмена'}
 	]);
 	
-	bot.on('message', msg => {
+	bot.on('message', async msg => {
 		const text = msg.text;
 		const chatId = msg.chat.id;
 	
 		if (text === '/start') {
-			return bot.sendMessage(chatId, 'Привет поц');
+			return bot.sendMessage(chatId, 'Привет, поц');
 		}
 	
 		if (text === '/info') {
-			return bot.sendMessage(chatId, 'Скоро здесь будет курс обмена');
+			let answer;
+
+			try {
+				const results = await getData();
+
+				const rubRate = results[0].data[0].adDetailResp.price;
+				const thbRate = results[1].data[0].adv.price;
+
+				answer = `Курс обмена Рубль-Бат\n\nБинанс: 1 бат от ${(rubRate / thbRate).toFixed(2)}`;
+			} catch {
+				answer = 'Чёт не то, попробуй позже';
+			}
+
+			return bot.sendMessage(chatId, answer);
 		}
 	
 		return bot.sendMessage(chatId, 'Не надо мне ничего писать, просто выбери команду');
